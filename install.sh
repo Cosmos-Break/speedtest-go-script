c3793b434e0bfbbe8bb066e3bf6ab8a4e030291c
Cosmos-Break <438854233@qq.com>
2022-09-05 16:18:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ function get_speedtest(){
     install_go
     cd && git clone https://github.com/librespeed/speedtest-go.git
     cd speedtest-go
-    mkdir $dir && cp -r settings.toml assets $dir
+    mkdir $dir && cp -r settings.toml $dir
     /usr/local/go/bin/go build -o speedtest main.go
     cp ./speedtest $dir
     cd && rm -rf speedtest go
